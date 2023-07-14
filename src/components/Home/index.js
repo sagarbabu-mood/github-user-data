bfebf5e7311135efe86b776b1e997b7f927511c9
Sagar Babu <sagarbabusv180@gmail.com>
2023-07-14 23:54:14 +0530

    updates

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,33 @@
-import Cookies from 'js-cookie';
-import { Redirect } from 'react-router-dom'
-import './index.css';
+import Cookies from "js-cookie";
+import { Redirect } from "react-router-dom";
+import "./index.css";
 
-const HomePage = () => {
-    const jwtToken = Cookies.get("sagar_token")
-    console.log(jwtToken)
+const HomePage = (props) => {
+    const onClickLogout = () => {
+        Cookies.remove("sagar_token");
+        const { history } = props;
+        history.replace("/");
+    };
+
+    const jwtToken = Cookies.get("sagar_token");
+    console.log(jwtToken);
     if (jwtToken === undefined) {
-        return <Redirect to="/register-and-login" />
+        return <Redirect to="/register-and-login" />;
     }
 
     return (
         <div className="home-container">
             <div className="text-container">
-                <h1 className="scrolling-text">
-                    Hey, Hi!... Welcome to my page
-                </h1>
+                <h1 className="scrolling-text">Hey, Hi!... Welcome to my page</h1>
             </div>
             <div className="emoji-container">
                 <span className="emoji">😃</span>
                 <span className="emoji">🎉</span>
                 <span className="emoji">🌈</span>
             </div>
+            <button className="logout-button" onClick={onClickLogout}>
+                Logout
+            </button>
         </div>
     );
 };
